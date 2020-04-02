1a2aefdee6043884263a23d5a0ec34d831e46409
Bistra Chilikova <bistra.chilikova@gmail.com>
2020-04-02 22:11:27 +0300

    add form update

--- src/components/addForm.js
+++ src/components/addForm.js
@@ -11,8 +11,22 @@ class AddForm extends React.Component
         this.state = {recipeId:this.props.id}
         this.handleChange = this.handleChange.bind(this);
         this.handleComponentChange = this.handleComponentChange.bind(this);
+        this.fetchCollections = this.fetchCollections.bind(this);
+        this.fetchCollections(1);
 
 
+    }
+
+    fetchCollections(userID)
+    {
+        //let fakeURL = 'https://5e7ce6e0a917d700166840b4.mockapi.io/postsByRecipe/'+recipeId
+        let realURL = "https://cook-me.herokuapp.com/users/"+userID+"/collections"
+        fetch(realURL)
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ collections: data }, ()=>console.log(this.state))
+      })
+        .catch(console.log)
     }
 
     handleComponentChange(nameOfField, valueOfField)
@@ -37,30 +51,24 @@ class AddForm extends React.Component
 
     render()
     {
+        if (this.state.collections == null)
+        {
+            return null;
+        }
         return(
-
-
             <>
                 <p class="h4 mb-4">Add to collection</p>
                 <div class="btn-group-vertical w-100 ">
-                    <button type="button" class="btn btn-outline-primary btn-lg btn-block text-left my-1">
-                    <svg class="bi bi-plus" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
-                        <path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
-                    </svg> Want to do
-                    </button>
-                    <button type="button" class="btn btn-outline-primary btn-lg btn-block text-left">
-                    <svg class="bi bi-plus" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
-                        <path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
-                    </svg> Favorites
-                    </button>
-                    <button type="button" class="btn btn-outline-primary btn-lg btn-block text-left my-1">
-                    <svg class="bi bi-plus" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
-                        <path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
-                    </svg> Desserts
-                    </button>
+                    {
+                        this.state.collections.map((collection) => (
+                            <button type="button" class="btn btn-outline-primary btn-lg btn-block text-left my-1">
+                            <svg class="bi bi-plus" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M8 3.5a.5.5 0 01.5.5v4a.5.5 0 01-.5.5H4a.5.5 0 010-1h3.5V4a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+                                <path fill-rule="evenodd" d="M7.5 8a.5.5 0 01.5-.5h4a.5.5 0 010 1H8.5V12a.5.5 0 01-1 0V8z" clip-rule="evenodd"/>
+                            </svg> {collection.name}
+                            </button>
+                        ))
+                    }
                     <div class="input-group my-1">
                   <div class="input-group-prepend ">
                     <button class="btn btn-secondary" type="button">
